62b1b342e42e3d16218fbb371af90befe3600aad
faesslerm <markus.mfs@gmail.com>
2023-04-03 19:59:14 +0200

    feat: modify style of login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,29 +1,33 @@
 <template>
-  <v-card class="mx-auto" style="max-width: 680px" color="accent">
-    <v-card-title>
-      <v-img class="mx-auto" width="90" src="@/assets/logo.svg"
-    /></v-card-title>
+  <v-sheet class="ma-auto pa-10" color="accent">
+    <v-img class="mx-auto mb-16" width="90" src="@/assets/logo.svg" />
 
-    <v-card-text style="max-width: 360px" class="mx-auto">
-      <div class="text-h3 font-weight-bold text-white">Login</div>
+    <v-sheet class="mx-auto" color="transparent" style="max-width: 360px">
+      <div class="text-h3 font-weight-bold text-white mb-5">Login</div>
 
       <v-form ref="form" @submit.prevent>
-        <v-text-field v-model="email" label="Email" type="email"></v-text-field>
+        <v-text-field
+          v-model="email"
+          label="Email"
+          type="email"
+          bg-color="white"
+        ></v-text-field>
         <v-text-field
           v-model="password"
           label="Password"
           type="password"
+          bg-color="white"
         ></v-text-field>
         <v-btn block prepend-icon="mdi-lock" size="x-large" color="primary"
           >Login</v-btn
         >
       </v-form>
 
-      <v-btn block size="small" variant="text" color="primary"
+      <v-btn block size="small" variant="text" color="primary" class="mt-5"
         >Forgot Password?</v-btn
       >
-    </v-card-text>
-  </v-card>
+    </v-sheet>
+  </v-sheet>
 </template>
 
 <script>
