688ab4f583355ce2866cbf25992a2041973e44d9
faesslerm <markus.mfs@gmail.com>
2023-04-03 20:00:30 +0200

    feat: add picture to login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,18 @@
 <template>
-  <LoginForm />
+  <v-container style="height: 100%">
+    <v-row align="center" style="height: 100%">
+      <v-col>
+        <LoginForm />
+      </v-col>
+      <v-col>
+        <v-img
+          class="mx-auto mb-16"
+          width="400"
+          src="@/assets/login-amico.svg"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
