98f0b2ce23632c4a74c3d72984ac2e666e3d38b3
faesslerm <markus.mfs@gmail.com>
2023-04-03 20:01:06 +0200

    feat: add linear gradient background to view layout

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,9 +1,20 @@
 <template>
-  <v-main>
+  <v-main class="main">
     <router-view />
   </v-main>
 </template>
 
 <script setup>
-  //
+//
 </script>
+
+<style scoped>
+.main {
+  background: linear-gradient(
+    112.52deg,
+    #25303e -3.84%,
+    #25303e 52.41%,
+    #712f2e 100%
+  );
+}
+</style>
