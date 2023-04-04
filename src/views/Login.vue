4808cf69e63b3d0e544fe434233704c015a2787a
faesslerm <markus.mfs@gmail.com>
2023-04-04 18:56:02 +0200

    feat: add some styling to login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-container style="height: 100%">
+  <v-container style="height: 100%" fluid class="pa-16">
     <v-row align="center" style="height: 100%">
       <v-col>
-        <LoginForm />
+        <login-form />
       </v-col>
       <v-col>
         <v-img
           class="mx-auto mb-16"
-          width="400"
+          width="50vh"
           src="@/assets/login-amico.svg"
         />
       </v-col>
