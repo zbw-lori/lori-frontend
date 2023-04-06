5060614b44b90be8d26894e0456e9a3b4e32e602
faesslerm <markus.mfs@gmail.com>
2023-04-06 18:34:38 +0200

    feat(navigation): add navigation layout

---
+++ src/layouts/default/Navigation.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <app-bar />
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+  import AppBar from './AppBar.vue'
+  import DefaultView from './View.vue'
+</script>
