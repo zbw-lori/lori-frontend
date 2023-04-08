3a0c0d133036fa679a829a248f76eddcf14f2978
faesslerm <markus.mfs@gmail.com>
2023-04-08 06:50:05 +0200

    feat(navigation): modify style of navigation drawer

--- src/layouts/default/Navigation.vue
+++ src/layouts/default/Navigation.vue
@@ -1,8 +1,9 @@
 <template>
   <v-app class="app">
     <v-navigation-drawer color="transparent" floating permanent class="pt-10 pl-10">
-      <v-list density="compact" nav color="white" class="pa-0">
-        <v-list-item class="pt-0"><v-img class="mb-5" width="70" src="@/assets/logo.svg" /></v-list-item>
+      <v-list density="compact" nav class="pa-0" active-color="primary" variant="flat">
+        <v-list-item class="pt-0" style="background-color: transparent"><v-img class="mb-5" width="70"
+            src="@/assets/logo.svg" /></v-list-item>
         <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" value="dashboard"></v-list-item>
         <v-list-item prepend-icon="mdi-robot" title="Robots" value="robots"></v-list-item>
         <v-list-item prepend-icon="mdi-file-tree" title="Stock Management" value="stock"></v-list-item>
