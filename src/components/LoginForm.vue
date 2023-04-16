092dbd823cd75cc81c31080256eab84f6b0a4acb
faesslerm <markus.mfs@gmail.com>
2023-04-16 08:32:08 +0200

    feat(login): modify style of login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,31 +1,17 @@
 <template>
-  <v-sheet class="pa-10" color="accent" style="max-width: 680px">
-    <v-img class="mx-auto mb-16" width="90" src="@/assets/logo.svg" />
+  <v-sheet color="accent" style="max-width: 680px; border-radius: 24px; padding: 120px 160px;">
+    <v-img class="mx-auto" width="90" style="margin-bottom: 80px" src="@/assets/logo.svg" />
 
     <v-sheet class="mx-auto" color="transparent" style="max-width: 360px">
       <div class="text-h3 font-weight-bold text-white mb-5">Login</div>
 
       <v-form ref="form" @submit.prevent>
-        <v-text-field
-          v-model="email"
-          label="Email"
-          type="email"
-          bg-color="white"
-        ></v-text-field>
-        <v-text-field
-          v-model="password"
-          label="Password"
-          type="password"
-          bg-color="white"
-        ></v-text-field>
-        <v-btn block prepend-icon="mdi-lock" size="x-large" color="primary"
-          >Login</v-btn
-        >
+        <v-text-field v-model="email" label="Email" type="email" bg-color="white"></v-text-field>
+        <v-text-field v-model="password" label="Password" type="password" bg-color="white"></v-text-field>
+        <v-btn block prepend-icon="mdi-lock" size="x-large" color="primary">Login</v-btn>
       </v-form>
 
-      <v-btn block size="small" variant="text" color="primary" class="mt-5"
-        >Forgot Password?</v-btn
-      >
+      <v-btn block size="small" variant="text" color="primary" class="mt-5">Forgot Password?</v-btn>
     </v-sheet>
   </v-sheet>
 </template>
