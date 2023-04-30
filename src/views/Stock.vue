1c0816c65a8c3d1f7981f84889b25fa02b3db557
Markus Faessler <markus.faessler@abacus.ch>
2023-04-30 18:18:13 +0200

    feat: add crudTable to stock view

--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="text-h3 font-weight-bold text-white ma-5" style="min-height: 900px; border-radius: 24px;padding: 80px;">
-    Stock Management
+  <div class="ma-5" style="min-height: 900px; border-radius: 24px; padding: 80px">
+    <div class="text-h3 font-weight-bold text-white mb-5">Stock Management</div>
+    <v-container style="margin: 0px">
+      <v-row justify="start">
+        <CrudTable />
+      </v-row>
+    </v-container>
   </div>
 </template>
 
-<script setup>
+<script>
+import CrudTable from '@/components/CrudTable.vue';
+
+export default {
+  components: {
+    CrudTable
+  }
+}
 </script>
