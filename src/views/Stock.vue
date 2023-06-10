30ee1d90f519492c4f44e4ffcff0e4c000ceee23
faesslerm <markus.mfs@gmail.com>
2023-06-10 10:25:13 +0200

    feat: add address table to stock view

--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="ma-5" style="min-height: 900px; border-radius: 24px; padding: 80px">
     <div class="text-h3 font-weight-bold text-white mb-5">Stock Management</div>
-    <v-container style="margin: 0px">
+    <v-container style="margin: 0;">
       <v-row>
         <OrderTable />
       </v-row>
       <v-row>
-        <CustomerTable />
+        <CustomerTable class="mt-2" />
+      </v-row>
+      <v-row>
+        <AddressTable class="mt-2" />
       </v-row>
     </v-container>
   </div>
@@ -15,11 +18,13 @@
 <script>
 import OrderTable from '@/components/OrderTable.vue';
 import CustomerTable from '@/components/CustomerTable.vue';
+import AddressTable from '@/components/AddressTable.vue';
 
 export default {
   components: {
     OrderTable,
-    CustomerTable
+    CustomerTable,
+    AddressTable,
   },
 }
 </script>
