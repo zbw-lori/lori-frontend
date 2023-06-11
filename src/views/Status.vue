552c74b44961a0bd8ea770fff2cec22e3cca0276
faesslerm <markus.mfs@gmail.com>
2023-06-11 10:30:46 +0200

    feat: add table to status view

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,8 +1,61 @@
 <template>
-  <div class="text-h3 font-weight-bold text-white ma-5" style="min-height: 900px; border-radius: 24px;padding: 80px;">
-    Customer Status
+  <div class="ma-5" style="min-height: 900px; border-radius: 24px; padding: 80px">
+    <div class="text-h3 font-weight-bold text-white mb-5">Customer Status</div>
+    <v-container style="margin: 0px; padding: 0;">
+      <v-table fixed-header>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Id
+            </th>
+            <th class="text-left">
+              Status
+            </th>
+            <th class="text-left">
+              Receipt Type
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.status }}</td>
+            <td>{{ item.receiptType }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-container>
   </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  data: () => ({
+    apiPath: `${import.meta.env.VITE_API_URL}/Order/status`,
+    items: [],
+  }),
+
+  async created() {
+    await this.callInit();
+  },
+
+  methods: {
+    async callInit() {
+      console.log(`Get status -> ${this.apiPath}`);
+      var jwt = localStorage.getItem("jwt");
+      var response = await fetch(`${this.apiPath}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+
+      var json = await response.json();
+      console.log(json);
+      this.items = json;
+    },
+  }
+}
 </script>
