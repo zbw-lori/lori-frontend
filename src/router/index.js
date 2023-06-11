6e8209444b49bc412aee31df11b763ffd2dbee9e
faesslerm <markus.mfs@gmail.com>
2023-06-11 13:05:52 +0200

    feat: add route guard if not authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 
+function isAuthenticated(){
+  return window.localStorage.getItem('jwt');
+}
+
 const routes = [
   {
     path: '/',
@@ -65,6 +69,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !isAuthenticated()) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
 })
 
 export default router
