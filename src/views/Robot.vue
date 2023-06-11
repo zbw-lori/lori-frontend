1ebf082f62ce0a84611b4146a6fec7e600e81663
faesslerm <markus.mfs@gmail.com>
2023-06-11 07:21:19 +0200

    feat: add robot table to robot view

--- src/views/Robot.vue
+++ src/views/Robot.vue
@@ -3,41 +3,18 @@
     <div class="text-h3 font-weight-bold text-white mb-5">Robots</div>
     <v-container style="margin: 0px">
       <v-row justify="start">
-        <CrudTable title="Robot" :headers="headers" :defaultItem="defaultItem" :apiPath="apiPath" />
+        <RobotTable />
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
-import CrudTable from '@/components/CrudTable.vue';
+import RobotTable from '@/components/RobotTable.vue';
 
 export default {
   components: {
-    CrudTable
+    RobotTable,
   },
-  data: () => ({
-    apiPath: `${import.meta.env.VITE_API_URL}/Robot`,
-    headers: [
-      {
-        title: 'Robot',
-        align: 'start',
-        sortable: false,
-        key: 'id',
-      },
-      { title: 'Name', key: 'name' },
-      { title: 'Description', key: 'description' },
-      { title: 'Model', key: 'model' },
-      // { title: 'Available', key: 'isAvailable' },
-      { title: 'Actions', key: 'actions', sortable: false },
-    ],
-    defaultItem: {
-      id: 0,
-      name: "",
-      description: "",
-      model: "",
-      isAvailable: false,
-    },
-  }),
 }
 </script>
