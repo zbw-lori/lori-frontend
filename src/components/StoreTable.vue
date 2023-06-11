5029c928d55662de17bf3b5bb7a5224ead024ac8
faesslerm <markus.mfs@gmail.com>
2023-06-11 07:11:00 +0200

    feat: add component store table

---
+++ src/components/StoreTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <CrudTable title="Store" :headers="headers" :defaultItem="defaultItem" :apiPath="apiPath" />
+</template>
+
+<script>
+import CrudTable from './CrudTable.vue';
+
+export default {
+  components: {
+    CrudTable
+  },
+  data: () => ({
+    apiPath: `${import.meta.env.VITE_API_URL}/Store`,
+    headers: [
+      { title: 'Id', key: 'id' },
+      { title: 'Name', key: 'name' },
+      { title: 'Actions', key: 'actions', sortable: false },
+    ],
+    defaultItem: {
+      id: 0,
+      name: "",
+    },
+  }),
+}
+</script>
