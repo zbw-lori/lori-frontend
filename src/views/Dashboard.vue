9fe7d42d7f90d17a9844947e0feefacbdd6a6fa9
faesslerm <markus.mfs@gmail.com>
2023-06-18 08:16:11 +0200

    feat: add sample chart on dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="text-h3 font-weight-bold text-white ma-5" style="min-height: 900px; border-radius: 24px;padding: 80px;">
-    Dashboard
+  <div class="ma-5" style="min-height: 900px; border-radius: 24px; padding: 80px">
+    <div class="text-h3 font-weight-bold text-white mb-5">Dashboard</div>
+    <Chart chartType="line" :chartData="chartData" :chartOptions="chartOptions" />
   </div>
 </template>
 
-<script setup>
+<script>
+import Chart from '@/components/Chart.vue';
+
+export default {
+  components: {
+    Chart,
+  },
+
+  data: () => ({
+    chartData: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: 'Capacity',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        borderColor: '#D17077',
+        backgroundColor: '#fff',
+      }]
+    },
+
+    chartOptions: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+      },
+    },
+  }),
+}
 </script>
