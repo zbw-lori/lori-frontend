e68ee5552672d07fdbbf67f79df88a10dfd0a2a3
faesslerm <markus.mfs@gmail.com>
2023-06-18 08:15:36 +0200

    feat: create generic chart component

---
+++ src/components/Chart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="background-color: white; border-radius: 10px; padding: 10px;">
+    <canvas></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  props: {
+    chartType: String,
+    chartData: Object,
+    chartOptions: Object,
+  },
+
+  mounted() {
+    let { chartType, chartData, chartOptions } = this;
+    this.initChart(chartType, chartData, chartOptions);
+  },
+
+  methods: {
+    initChart(chartType, chartData, chartOptions) {
+      const chartElement = document.querySelector("canvas");
+      new Chart(chartElement, {
+        type: chartType,
+        data: chartData,
+        options: chartOptions,
+      });
+    },
+  },
+}
+</script>
